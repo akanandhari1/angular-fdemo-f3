122b6867fefa84fd59909eeec981a92cbf3de8ce
akanandhari1 <akanandhari@hotmail.com>
2023-05-06 11:50:17 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,22 @@ import { DcRegisterComponent } from './DC/dc-register/dc-register.component';
 import { DcFetchDataComponent } from './DC/dc-fetch-data/dc-fetch-data.component';
 import { BrowseFileComponent } from './shared/browse-file/browse-file.component';
 import { EditDcComponent } from './DC/edit-dc/edit-dc.component';
-
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -102,7 +117,13 @@ import { EditDcComponent } from './DC/edit-dc/edit-dc.component';
   providers: [
     ConfirmationDialogService,
     { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
 
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
     {
       provide: NG_VALIDATORS,
       useExisting: forwardRef(() => MultiselectAutoCompleteComponent),
